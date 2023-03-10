742ef1f8457adc83382e4a1ac3d6764e8f8fd7a1
rahulkulmi <rahul@newput.com>
2023-03-10 21:45:43 +0530

    Added changes for test ai.

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.get('/', function (req, res) {
 
 app.get('/user', function (req, res) {
     res.send('Main page user get api.');
-  });
+});
+
+app.get('/test', function (req, res) {
+  res.send('Main page test get api.');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
