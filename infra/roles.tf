cb14797643faab0e3abe28b6e18819497bef0037
rahulkulmi <rahul@newput.com>
2023-03-13 10:46:13 +0530

    Added changes for formate.

--- infra/roles.tf
+++ infra/roles.tf
@@ -2,8 +2,8 @@
 data "aws_iam_policy_document" "ecs_task_execution_assume_role" {
   version = "2012-10-17"
   statement {
-    sid = ""
-    effect = "Allow"
+    sid     = ""
+    effect  = "Allow"
     actions = ["sts:AssumeRole"]
 
     principals {
@@ -67,6 +67,6 @@ resource "aws_iam_policy" "ecs_task_execution_role" {
 # ECS task execution role policy attachment
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  # policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
   policy_arn = aws_iam_policy.ecs_task_execution_role.arn
+  # policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
